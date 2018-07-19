1bfcabf0f58e4b65f196fcc2943440e843e6c516
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 02:22:19 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+def reverse_each_word(sentence)
+  split_sentence = sentence.split(" ")
+
+  reversed = []
+  split_sentence.each do |word|
+    reversed << word.reverse
+  end
+
+  reversed.join(" ")
+end
+
+def reverse_each_word(sentence)
+  split_sentence = sentence.split(" ")
+
+  reversed = []
+  split_sentence.collect do |word|
+    reversed << word.reverse
+  end
+
+  reversed.join(" ")
+end
